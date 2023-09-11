856939327beec62c131be18987718f6d9a9f643c
sydevops85 <144699848+sydevops85@users.noreply.github.com>
2023-09-11 20:52:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,8 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado',
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
-streamlit.dataframe[dataframe(my_fruit_list).index.isin(list(streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Banana'])))]
+df = dataframe(my_fruit_list)
+list = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Banana'])
+streamlit.list
+
+# df[df.index.isin(list)]
